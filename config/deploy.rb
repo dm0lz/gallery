eb705731d49226d0435da1fa58446e4a2e63206c
oducroux <ducrouxolivier@gmail.com>
2013-08-09 14:46:19 +0200

    ii

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,6 +18,8 @@ set :user, "olivierdoe"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :use_sudo, false
 
+set :shared_children, shared_children + %w{public/uploads}
+
 set :scm, "git"
 set :repository,  "https://github.com/oducroux/#{application}.git"
 set :branch, "master"
